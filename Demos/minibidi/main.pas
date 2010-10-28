3a64ed3eb38541d2a8cdc31a961946ba549bfbdb
parmaja <parmaja@339e1bde-22b9-40eb-b2ea-6a9265cf6bc7>
2010-10-28 13:23:25 +0000

    Add: minibidi demo

---
+++ Demos/minibidi/main.pas
@@ -0,0 +1,70 @@
+unit main;
+{$codepage utf8}
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls,
+  minibidi;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Panel1: TPanel;
+    procedure Button1Click(Sender: TObject);
+  private
+  public
+    procedure Draw(s: WideString);
+  end;
+
+var
+  Form1: TForm1; 
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  w: widestring;
+  pw: PWideChar;
+begin
+  Panel1.Canvas.Clear;
+  w := UTF8Decode(Edit1.Text);
+  pw := PWideChar(w);
+  DoBidi(pw, Length(w), false, true);
+  Draw(w);
+end;
+
+procedure TForm1.Draw(s: WideString);
+var
+  c: WideChar;
+  t: widestring;
+  u: utf8string;
+  i: Integer;
+  x, y, w: Integer;
+  st: TTextStyle;
+begin
+  x := 0;
+  y := 50;
+  FillChar(st, SizeOf(st),#0);
+  for i := 1 to Length(s) do
+  begin
+    t := s[i];
+    u := UTF8Encode(t);
+    w := Panel1.Canvas.TextWidth(u);
+    Panel1.Canvas.TextRect(Panel1.ClientRect, x, y, u, st);
+    x := x + w;
+  end;
+end;
+
+end.
+
