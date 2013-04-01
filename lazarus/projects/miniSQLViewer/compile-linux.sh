4d31c0dc920a51efe41b6da8e8ed87f8821ebe69
parmaja <parmaja@339e1bde-22b9-40eb-b2ea-6a9265cf6bc7>
2013-04-01 17:34:39 +0000

    Move miniSQlViewer to subproject at sourceforge /minilib/sqlviewer

--- lazarus/projects/miniSQLViewer/compile-linux.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if fpc gui/SqliteViewer.lpr @linux.cfg -obin/sqliteviewer -d%1  -B -dLCL -FUunits/; then
-  echo '##### Compile #####'
-else
-  echo '????? Error  ?????'  
-  exit
-fi
-echo 'Press Enter to exit.'
-read
--- lazarus/projects/miniSQLViewer/copy-pocketpc.cmd
+++
@@ -1,3 +0,0 @@
-cecopy /s bin\wince\sqliteviewer.exe "dev:\Storage Card\Sqlite\sqliteviewer.exe"
-cecopy /s bin\wince\sqlite3.dll "dev:\Storage Card\Sqlite\sqlite3.dll"
-pause
