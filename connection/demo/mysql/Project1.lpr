6e945384bc3c429a622135cb4bdaa7e9ca5d2d58
Zaher Dirkey <zaherdirkey@yahoo.com>
2015-09-10 22:52:23 +0300

    MySQL: add: MySQL DEMO

---
+++ connection/demo/mysql/Project1.lpr
@@ -0,0 +1,19 @@
+program Project1;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, Forms, Unit1;
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource := True;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+
---
+++ connection/demo/mysql/Unit1.pas
@@ -0,0 +1,295 @@
+unit Unit1;
+{$M+}
+{$H+}
+{$IFDEF FPC}
+{$mode delphi}
+{$ENDIF}
+
+interface
+
+uses     
+  SysUtils, Variants, Classes, Graphics, Controls, Forms, mncPostgre, mncSQLite,
+  mysql50conn,
+  Dialogs, StdCtrls, mncConnections, mncMySQL, ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    CreateBtn: TButton;
+    CreateBtn1: TButton;
+    SelectBtn: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    ListBox1: TListBox;
+    Image1: TImage;
+    SelectDSBtn: TButton;
+    procedure Button2Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure CreateBtn1Click(Sender: TObject);
+    procedure CreateBtnClick(Sender: TObject);
+    procedure SelectBtnClick(Sender: TObject);
+    procedure SelectDSBtnClick(Sender: TObject);
+  private
+  public
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  Conn: TmncMySQLConnection;
+  Session: TmncMySQLSession;
+  Cmd: TmncMySQLCommand;
+begin
+  Conn := TmncMySQLConnection.Create;
+  try
+    Conn.Resource := 'test';
+    Conn.UserName := 'test';
+    Conn.Password := 'test'; //not a real password man!
+    Conn.Connect;
+    Session := TmncMySQLSession.Create(Conn);
+    try
+      Session.Start;
+      Cmd :=  TmncMySQLCommand.Create(Session);
+      try
+        
+        Cmd.SQL.Text := 'insert into companies';
+        Cmd.SQL.Add('(id, name, nationality)');
+        Cmd.SQL.Add('values (?id, ?name, ?nationality)');
+        Cmd.Prepare;
+        Cmd.Params['id'].AsInt64 := 1;
+        Cmd.Params['name'].AsString := 'zaher';
+        Cmd.Params['nationality'].AsString := 'syria';
+
+        Cmd.Execute;
+
+        Cmd.Params['id'].AsInt64 := 2;
+        Cmd.Params['name'].AsString := 'dirkey';
+        Cmd.Params['nationality'].AsString := 'damascus';
+
+        Cmd.Execute;
+
+      finally
+        Cmd.Free;
+      end;
+    finally
+      Session.Free;
+    end;
+  finally
+    Conn.Free;
+  end;
+end;
+
+procedure TForm1.CreateBtn1Click(Sender: TObject);
+var
+  Conn: TmncMySQLConnection;
+  Session: TmncMySQLSession;
+  Cmd: TmncMySQLCommand;
+begin
+  Conn := TmncMySQLConnection.Create;
+  try
+    Conn.Resource := 'test';
+    Conn.UserName := 'test';
+    Conn.Password := 'test'; //not a real password man!
+    Conn.Connect;
+    Conn.Execute('drop  database if exists test2');
+    ShowMessage('Database dropped');
+  finally
+    Conn.Free;
+  end;
+end;
+
+procedure TForm1.CreateBtnClick(Sender: TObject);
+var
+  Conn: TmncMySQLConnection;
+  s: string;
+begin
+  Conn := TmncMySQLConnection.Create;
+  try
+    Conn.UserName := 'test';
+    Conn.Password := 'test'; //not a real password man!
+    Conn.Connect;
+    //Conn.Execute('create database if not exists test2');
+    Conn.SelectDatabase('test2');
+
+    s := 'create table companies ('#13#10+
+    '  id int not null auto_increment,'#13#10+
+    '  name varchar(50) default null,'#13#10+
+    '  nationality varchar(50) default null,'#13#10+
+    '  start_on date,'#13#10+
+    '  end_on timestamp,'#13#10+
+    '  money decimal(10,2) default null,'#13#10+
+    '  is_tested bit default null,'#13#10+
+    '  primary key(id)'#13#10+
+    ')'#13#10+
+    'collate=''utf8_general_ci'''#13#10+
+    'engine=innodb'#13#10+
+    'auto_increment=3'#13#10;
+
+    Conn.Execute(s);
+
+    ShowMessage('Database Created');
+  finally
+    Conn.Free;
+  end;
+end;
+
+procedure TForm1.SelectBtnClick(Sender: TObject);
+var
+  Conn: TmncMySQLConnection;
+  Session: TmncMySQLSession;
+  Cmd: TmncMySQLCommand;
+  field: string;
+begin
+  ListBox1.Clear;
+  Conn := TmncMySQLConnection.Create;
+  try
+    Conn.Resource := 'test';
+    Conn.UserName := 'test';
+    Conn.Password := 'test'; //not a real password man!
+    Conn.Connect;
+    Session := TmncMySQLSession.Create(Conn);
+    Session.Start;
+    try
+      field := 'money';
+      Cmd := Session.CreateCommand as TmncMySQLCommand;
+      Cmd.SQL.Text := 'select '+field+' from companies';
+      //Cmd.SQL.Add('where ID = ?ID');
+      Cmd.Prepare;
+      Cmd.NextOnExecute := True;
+      //Cmd.Param['ID'].AsInt64 := 2;
+      if Cmd.Execute then
+      begin
+        while not Cmd.EOF do
+        begin
+          //ListBox1.Items.Add(Cmd.Field['name'].Column.Name + ': ' + Cmd.Field['name'].AsString);
+          ListBox1.Items.Add(Cmd.Field[field].Column.Name + ': ' + Cmd.Field[field].AsString);
+          Cmd.Next;
+        end;
+      end;
+      Cmd.Close;
+    finally
+      Session.Free;
+    end;
+    Conn.Disconnect;
+  finally
+    Conn.Free;
+  end;
+end;
+
+procedure TForm1.SelectDSBtnClick(Sender: TObject);
+var
+  Conn: TmncMySQLConnection;
+  Session1: TmncMySQLSession;
+  Session2: TmncMySQLSession;
+  Cmd1, Cmd2: TmncMySQLCommand;
+  c: Currency;
+begin
+  ListBox1.Clear;
+  Conn := TmncMySQLConnection.Create;
+  try
+    Conn.Resource := ExpandFileName(ExtractFilePath(Application.ExeName) + '..\data\cars.MySQL');
+    Conn.Connect;
+    Session1 := TmncMySQLSession.Create(Conn);
+    Session1.Start;
+    try
+      Cmd1 := Session1.CreateCommand as TmncMySQLCommand;
+      Cmd1.SQL.Text := 'select * from companies';
+      Cmd1.SQL.Add('where name = ?name');
+      Cmd1.Prepare;
+      Cmd1.Param['name'].AsString := 'Ferrari';
+      if Cmd1.Execute then
+      begin
+        while not Cmd1.EOF do
+        begin
+          ListBox1.Items.Add(Cmd1.Field['name'].AsString);
+          Cmd1.Next;
+        end;
+      end;
+      //Start Session2
+      Session2 := TmncMySQLSession.Create(Conn);
+  //    Session2.Start;
+      try
+        Cmd2 := Session2.CreateCommand as TmncMySQLCommand;
+        Cmd2.SQL.Text := 'select * from companies';
+        Cmd2.SQL.Add('where name = ?name');
+           Cmd2.Prepare;
+        Cmd2.Param['name'].AsString := 'Audi';
+        if Cmd2.Execute then
+        begin
+          while not Cmd2.EOF do
+          begin
+            ListBox1.Items.Add(Cmd2.Field['name'].AsString);
+            Cmd2.Next;
+          end;
+        end;
+        Cmd2.Close;
+      finally
+//        Session2.Commit;
+        Session2.Free;
+      end;
+      //End Session2
+      Cmd1.Close;
+    finally
+      Session1.Commit;
+      Session1.Free;
+    end;
+    Conn.Disconnect;
+  finally
+    Conn.Free;
+  end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+  Conn: TmncMySQLConnection;
+  Session: TmncMySQLSession;
+  Cmd: TmncMySQLCommand;
+  s: TStringStream;
+  im: string;
+begin
+  Conn := TmncMySQLConnection.Create;
+  try
+    Conn.Resource := ExpandFileName(ExtractFilePath(Application.ExeName) + '..\data\cars.MySQL');
+    Conn.AutoStart := True;
+    Conn.Connect;
+    Session := TmncMySQLSession.Create(Conn);
+    try
+      Cmd := TmncMySQLCommand.CreateBy(Session);
+      Cmd.SQL.Text := 'select * from companies';
+//      Cmd.SQL.Add('where name = ?name');
+      Cmd.Prepare;
+//      Cmd.Param['name'].AsString := 'Ferrari';
+      if Cmd.Execute then
+      begin
+        while not Cmd.EOF do
+        begin
+          ListBox1.Items.Add(Cmd.Field['id'].AsString + ' - ' + Cmd.Field['name'].AsString);
+          im := Cmd.Field['image'].AsString;
+          s := TStringStream.Create(im);
+          try
+            Image1.Picture.Bitmap.LoadFromStream(s);
+          finally
+            s.Free;
+          end;
+          Cmd.Next;
+        end;
+      end;
+      Cmd.Close;
+    finally
+      Session.Free;
+    end;
+    Conn.Disconnect;
+  finally
+    Conn.Free;
+  end;
+end;
+
+end.
