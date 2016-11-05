c4442e8c80a2ca572428bf365ca3d3282ee75b96
Zaher Dirkey <zaherdirkey@yahoo.com>
2016-11-05 18:54:05 +0200

    Add installer in Linux

---
+++ install.sh
@@ -0,0 +1,13 @@
+#Adding packages to Lazarus IDE
+set -e
+lazarusdir="../lazarus"
+opt="--lazarusdir=$lazarusdir"
+$lazarusdir/lazbuild $opt ./lib/MiniCommons.lpk
+$lazarusdir/lazbuild $opt ./xml/source/MiniXML.lpk
+$lazarusdir/lazbuild $opt ./socket/source/MiniSockets.lpk
+$lazarusdir/lazbuild $opt ./connection/src/MiniConnections.lpk
+$lazarusdir/lazbuild $opt ./comm/source/MiniComm.lpk
+$lazarusdir/lazbuild $opt ./lazarus/lib/MiniLib.lpk
+$lazarusdir/lazbuild $opt --add-package ./lazarus/components/native/NativeLib.lpk
+$lazarusdir/lazbuild --add-package ./pos/source/poslib.lpk
+$lazarusdir/lazbuild --build-ide=
