9bb05871cc779b365cb55742b644485d27f11e99
Zaher Dirkey <zaherdirkey@yahoo.com>
2019-02-05 23:50:27 +0200

    minor Fix compile

--- install.cmd
+++ install.cmd
@@ -3,7 +3,5 @@ lazbuild .\xml\source\MiniXML.lpk
 lazbuild .\socket\source\MiniSockets.lpk
 lazbuild .\connection\src\MiniConnections.lpk
 lazbuild .\comm\source\MiniComm.lpk
-lazbuild .\lazarus\lib\MiniLib.lpk
-lazbuild --add-package .\lazarus\components\native\NativeLib.lpk --add-package .\pos\source\poslib.lpk
-lazbuild --build-ide= 
+lazbuild --build-ide=
 pause
