7a116e877d22126c32044ba42cbabb6ad50bfaa4
Zaher Dirkey <zaherdirkey@yahoo.com>
2019-04-13 12:12:59 +0300

    Move GPSUtils.pas into dev directory

--- lib/dev/GPSUtils.pas
+++ lib/dev/GPSUtils.pas
@@ -46,7 +46,7 @@ interface
 
 uses
   Classes, SysUtils, SyncObjs,
-  mnStreams, mnCommClasses;
+  mnStreams, mnCommThreads, mnCommClasses;
   
 type
   EGPSError = class(Exception);
@@ -73,6 +73,21 @@ function GPSPrase(Command:string; var Info:TGPSInfo):Boolean;
 function GPSInfo: TGPSInfo;
 procedure SetGPSInfo(Info: TGPSInfo);
 function GPSLock: TCriticalSection;
+
+type
+  { TmnGPSThread }
+
+  TmnGPSThread = class(TmnCommThread)
+  private
+    FEndOfLine: string;
+    FBuffer: string;
+  protected
+    procedure StringArrived(S: string); override;
+    procedure Parse(S: string); virtual;
+  public
+    property EndOfLine: string read FEndOfLine write FEndOfLine;
+  end;
+
 
 implementation
 
@@ -207,6 +222,35 @@ begin
     FGPSInfo := Info;
   finally
     GPSLock.Leave;
+  end;
+end;
+
+procedure TmnGPSThread.StringArrived(S: string);
+var
+  P: Integer;
+begin
+  FBuffer := FBuffer + S;
+  P := AnsiPos(FEndOfLine, FBuffer);
+  while P > 0 do
+  begin
+    Parse(Copy(FBuffer, 1, P - 1));
+    FBuffer := Copy(FBuffer, P + 1, MaxInt);
+    P := AnsiPos(FEndOfLine, FBuffer);
+  end;
+end;
+
+procedure TmnGPSThread.Parse(S: string);
+var
+  Info:TGPSInfo;
+begin
+  if GPSPrase(S, Info) then
+  begin
+    SetGPSInfo(Info);
+//    if CommStream.Connected then
+//    begin
+//      FBuffer:='';
+//      CommStream.Purge;//try to clear cache, we need a runtime values from GPS receiver
+//    end;
   end;
 end;
 
