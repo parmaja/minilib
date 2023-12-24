7b484b15f08bf76717436e703fb42d67716a48a9
belalhamed <belalhamed@gmail.com>
2023-12-24 06:45:40 +0300

    [mnFields]: support data memory

--- lib/mnFields.pas
+++ lib/mnFields.pas
@@ -52,6 +52,11 @@ type
     function GetIField(const FieldName: string): IField;
   end;
 
+  TmncFieldMemory = record
+    Size: Cardinal;
+    Data: PByte;
+  end;
+
   { TmnCustomField }
 
   TmnCustomField = class abstract(TInterfacedPersistent, IField)
@@ -135,6 +140,7 @@ type
     function GetIsNull: Boolean; virtual;
     procedure SetIsNull(const AValue: Boolean); virtual;
     procedure Created; virtual;
+    function GetMemory: TmncFieldMemory; virtual;
   public
     procedure AfterConstruction; override;
 
@@ -183,6 +189,7 @@ type
     procedure Assign(Source: TPersistent); override;
     procedure Clear; virtual;//make value null //should be abstract
     procedure Empty; virtual;//make value empty
+    function Memory: TmncFieldMemory;
 
     //class operator Implicit (f: TprmustomField): Integer; { TODO : when support by delphi }
   end;
@@ -351,6 +358,12 @@ begin
   Result := (Self = nil) or (VarType(Value) in [varEmpty, varNull, varDispatch, varUnknown]);
 end;
 
+function TmnCustomField.GetMemory: TmncFieldMemory;
+begin
+  Result.Size := 0;
+  Result.Data := nil;
+end;
+
 procedure TmnCustomField.LoadFromFile(const FileName: string);
 var
   Stream: TStream;
@@ -366,6 +379,11 @@ end;
 procedure TmnCustomField.LoadFromStream(Stream: TStream);
 begin
   raise Exception.Create('Not implemented yet');
+end;
+
+function TmnCustomField.Memory: TmncFieldMemory;
+begin
+  Result := GetMemory;
 end;
 
 function TmnCustomField.ReadAsBoolean: Boolean;
