b9dea78acd97e9aa6077821bc87bae2b768f5952
belalhamed <belalhamed@gmail.com>
2024-09-02 07:34:53 +0300

    [UniDates]: mark udtISOStrToDate as deprecated duplicate in mnUtils

--- lib/UniDates.pas
+++ lib/UniDates.pas
@@ -129,8 +129,8 @@ function udtHourPeriodToString(vPeriod: Double): String;
 function udtDateToString(DateTime: TDateTime; Options: TUniviersalDateFlags = []): String; overload;
 function udtStringToDate(vStr: String): TDateTime;
 
-procedure udtISOStrToDate(ISODate: String; out Y, M, D, H, N, S: Word; vDateSeparator: Char = '-'; TimeDivider: Char = #0; UseDefault: Boolean = False); overload;
-function udtISOStrToDate(UDS: TUniviersalDateSystem; ISODate: String; vDateSeparator: Char = '-'; TimeDivider: Char = #0; UseDefault: Boolean = False): TDateTime; overload;
+procedure udtISOStrToDate(ISODate: String; out Y, M, D, H, N, S: Word; vDateSeparator: Char = '-'; TimeDivider: Char = #0; UseDefault: Boolean = False); overload; deprecated 'Use in mnUtils';
+function udtISOStrToDate(UDS: TUniviersalDateSystem; ISODate: String; vDateSeparator: Char = '-'; TimeDivider: Char = #0; UseDefault: Boolean = False): TDateTime; overload; deprecated 'Use in mnUtils';
 function udtISOStrToDate(ISODate: String; vDateSeparator: Char= '-'; TimeDivider: Char = #0; UseDefault: Boolean = False): TDateTime; overload;
 
 function udtISOCorrespondStr(DateTime: TDateTime; vDateSeparator: Char = '-'; TimeDivider: Char = ' '; WithTime: Boolean = False): String;
