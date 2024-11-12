a77495dabc35b3bddc43303c072a9aeec835ba75
Zaher Dirkey <zaherdirkey@yahoo.com>
2024-11-12 13:35:57 +0300

    update batch for download snapshot

--- snapshot/download_snapshot.cmd
+++ snapshot/download_snapshot.cmd
@@ -16,12 +16,16 @@ curl -C - --progress-bar --remote-name http://downloads.freepascal.org/fpc/snaps
 echo Downloading FPC Win32 compiler for windows
 curl -C - --progress-bar --remote-name http://downloads.freepascal.org/fpc/snapshot/trunk/i386-win32/fpc-3.3.1.i386-win32.zip
 
+echo Downloading FPC Win32 compiler for windows
+curl -C - --progress-bar --remote-name fpc-3.3.1.i386-win32.built.on.x86_64-linux.tar.gz
+
 echo Downloading FPC Win32 cross to Win64 for windows
 curl -C - --progress-bar --remote-name http://downloads.freepascal.org/fpc/snapshot/trunk/x86_64-win64/fpc-3.3.1.x86_64-win64.built.on.i386-win32.zip
 
-echo Downloading win64 compileer for windows
+echo Downloading win64 compiler for windows
 curl -C - --progress-bar --remote-name http://downloads.freepascal.org/fpc/snapshot/trunk/x86_64-win64/fpc-3.3.1.x86_64-win64.built.on.x86_64-linux.tar.gz
 
+echo Downloading binutils for windows
 curl -C - --progress-bar --remote-name http://downloads.freepascal.org/fpc/contrib/utils/win32/makew32.zip
 curl -C - --progress-bar --remote-name http://downloads.freepascal.org/fpc/contrib/cross/mingw/win64/binutils-2.21-win32-x86_64-win64.zip
 
--- snapshot/extract_snapshot_w64.cmd
+++ snapshot/extract_snapshot_w64.cmd
@@ -5,13 +5,21 @@ cd %d%
 
 7z x fpc-3.3.1.x86_64-win64.built.on.x86_64-linux.tar.gz -so | 7z x -si -ttar -o.\fpc\
 
-move /Y .\fpc\bin\*.* .\fpc\bin\x86_64-win64
+move /-Y .\fpc\bin\*.* .\fpc\bin\x86_64-win64
 
 7z x fpc.zip -o*
 rename .\fpc\fpc src
 
-7z x fpcbuild.zip -o.\fpc\ -y
+7z x fpcbuild.zip -o.\fpc\
 
 copy .\fpc\fpcbuild\install\binw64\*.* .\fpc\bin\x86_64-win64
 
 copy ..\makecfg.cmd .\fpc\bin\x86_64-win64
+
+cd .\fpc\bin\x86_64-win64
+
+rem run makecfg.cmd
+
+rem extract units from win32 fpc-3.3.1.i386-win32.zip
+
+rem maybe copy units win64 from win32 cross to win64
